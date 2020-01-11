3e9372818caf7ad53612d1af784aebb2d45d3fd9
herhor67 <herhor67@interia.pl>
2020-01-11 20:26:13 +0100

    Update Conjugate.m

--- OADM/Multivariable/Conjugate.m
+++ OADM/Multivariable/Conjugate.m
@@ -1,51 +1,58 @@
 clear all
 clc
 
-f = @(x) sin(x(1)-0.2)^2+sin(x(2)+0.3)^2;
-gf = @(x) [-2*sin(0.2-x(1))*cos(0.2-x(1)); 2*sin(x(2)+0.3)*cos(x(2)+0.3)];
+func = @(x, y) sin(x-0.2)^2+sin(y+0.3)^2;
+grad = @(x, y) [-2*sin(0.2-x)*cos(0.2-x) 2*sin(y+0.3)*cos(y+0.3)];
 
-xMin = -1; xMax = 1;
-yMin = -1; yMax = 1;
+Min = [-1 -1];
+Max = [ 1  1];
 
-x0 = [0; 0];
+p0 = [0 0];
 
-d1 = -gf(x0);
-avect = sort([(xMin-x0(1))/d1(1), (yMin-x0(2))/d1(2), (xMax-x0(1))/d1(1), (yMax-x0(2))/d1(2)]);
-alpha = fminbnd(@(a) f(x0+a*d1), avect(2), avect(3))
-x1 = x0+alpha*d1;
+%==============================================
+funchelper = @(p) func(p(1), p(2));
+gradhelper = @(p) grad(p(1), p(2));
+iters = 0;
+
+fprintf('Current point: (%f, %f)\n', p0);
+d1 = -gradhelper(p0);
+fprintf('Current dir:   [%f, %f]\n', d1);
+aMin = nanmax(((Min(1)-p0(1))/d1(1))*(d1(1)>=0)+((Max(1)-p0(1))/d1(1))*(d1(1)<=0), ((Min(2)-p0(2))/d1(2))*(d1(2)>=0)+((Max(2)-p0(2))/d1(2))*(d1(2)<=0));
+aMax = nanmin(((Min(1)-p0(1))/d1(1))*(d1(1)<=0)+((Max(1)-p0(1))/d1(1))*(d1(1)>=0), ((Min(2)-p0(2))/d1(2))*(d1(2)<=0)+((Max(2)-p0(2))/d1(2))*(d1(2)>=0));
+alpha = 0;
+if aMin <= aMax && ~isinf(aMin) && ~isinf(aMax)
+    alpha = fminbnd(@(a) funchelper(p0+a*d1), min(aMax, aMin), max(aMax, aMin));
+end
+fprintf('\nAlpha: %f E <%f, %f>\n\n', alpha, aMin, aMax);
+p1 = p0+alpha*d1;
 
 iterations=0;
 while true
-    x0
-    x1
+    fprintf('Current point: (%f, %f)\n', p1);
+    grad0 = gradhelper(p0);
+    grad1 = gradhelper(p1);
     
-    grad0 = gf(x0)
-    grad1 = gf(x1)
+    B1 = grad1' * (grad1 - grad0) / sum(abs(grad0).^2);
+    d2 = d1*B1 - grad1;
+    fprintf('Current dir:   [%f, %f]\n', d2);
     
-    B1 = grad1' * (grad1 - grad0) / sum(abs(grad0).^2)
+	aMin = nanmax(((Min(1)-p1(1))/d2(1))*(d2(1)>=0)+((Max(1)-p1(1))/d2(1))*(d2(1)<=0), ((Min(2)-p1(2))/d2(2))*(d2(2)>=0)+((Max(2)-p1(2))/d2(2))*(d2(2)<=0));
+    aMax = nanmin(((Min(1)-p1(1))/d2(1))*(d2(1)<=0)+((Max(1)-p1(1))/d2(1))*(d2(1)>=0), ((Min(2)-p1(2))/d2(2))*(d2(2)<=0)+((Max(2)-p1(2))/d2(2))*(d2(2)>=0));
+    alpha = 0;
+    if aMin <= aMax && ~isinf(aMin) && ~isinf(aMax)
+        alpha = fminbnd(@(a) funchelper(p1+a*d2), min(aMax, aMin), max(aMax, aMin));
+    end
+    fprintf('\nAlpha: %f E <%f, %f>\n\n', alpha, aMin, aMax);
     
-    d2 = B1*d1 - grad1
+    p2 = p1+alpha*d2;
+    p0 = p1;
+    p1 = p2;
+    iters = iters + 1;
     
-    avect = sort([(xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2), (xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2)]);
-    alpha = fminbnd(@(a) f(x1+a*d2), avect(2), avect(3))      % tit for tat
-
-%    aMin = max((xMax-x1(1))/d2(1), (yMax-x1(2))/d2(2))       % tit for tat
-%    aMax = min((xMin-x1(1))/d2(1), (yMin-x1(2))/d2(2))
-%    if aMin <= aMax
-%        alpha = fminbnd(@(a) f(x1+a*d2), aMin, aMax)
-%    else
-%        alpha = 0
-%    end
-    
-    x2 = x1+alpha*d2
-    x0 = x1;
-    x1 = x2;
-    
-    iterations = iterations+1;
-    
-    if (sqrt(sum(abs(alpha*d2).^2)) < 0.001)
+    if (sqrt(sum((alpha*d2).^2)) < 0.001)
         break
     end
 end
 
-iterations
+fprintf('Final point: (%f, %f)\n', p1);
+fprintf('Minimization took %i iterations.\n', iters);
