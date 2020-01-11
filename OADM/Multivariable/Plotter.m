232d2109368486893f8ba607fbfa4327ef4c6c97
herhor67 <herhor67@interia.pl>
2020-01-11 20:08:05 +0100

    Update Plotter.m

--- OADM/Multivariable/Plotter.m
+++ OADM/Multivariable/Plotter.m
@@ -1,16 +1,15 @@
 clear all
 clc
 
-f = @(x) sin(x(1)-0.2)^2+sin(x(2)+0.3)^2;
+func = @(x, y) sin(x-0.2)^2+sin(y+0.3)^2;
 
 xMin = -1; xMax = 1;
 yMin = -1; yMax = 1;
 
 %==============================================
 
-helper = @(x, y) f([x y]);
-[X, Y] = meshgrid(xMin:0.01:xMax, yMin:0.01:yMax);
-Z = arrayfun(helper, X, Y);
+[X, Y] = meshgrid(xMin:0.1:xMax, yMin:0.1:yMax);
+Z = arrayfun(func, X, Y);
 surf(X, Y, Z)
 colormap(jet)
 shading interp
