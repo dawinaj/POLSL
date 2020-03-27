a3e8cfeadfc060f8a34a1ec0878bfd0224c5145d
herhor67 <herhor67@interia.pl>
2020-03-27 12:53:16 +0100

    Update interpolation.m

--- NM/Lab3/interpolation.m
+++ NM/Lab3/interpolation.m
@@ -1,22 +1,43 @@
 clear all
 clc
 
-[Xi, Wn] = lagrangeInterpolation(-10, 10, 10, 3)
+a = -10;
+b = 10;
+n = 10;
+z = 3;
+
+pts = a:((b-a)/100):b;
+    
+figure;
+subplot(2,2,1)
+hold on
+plot(pts, arrayfun(@(x) f(x), pts))
+monInt = pts;
+for i = 1:length(pts)
+   [~, val] = monomialInterpolation(a, b, n, pts(i));
+   monInt(i) = val;
+end
+plot(pts, monInt)
+hold off
+subplot(2,2,2)
+hold on
+plot(pts, arrayfun(@(x) f(x), pts))
+lagInt = pts;
+for i = 1:length(pts)
+   [~, val] = monomialInterpolation(a, b, n, pts(i));
+   lagInt(i) = val;
+end
+plot(pts, lagInt)
+hold off
 
 function y = f(x)
     y = cos(x);
 end
 
 function [Xs, Wn] = lagrangeInterpolation(a, b, n, zeta)
-    Xs = ChebyshevNodes(a, b, n);    % get nodes
+    Xs = eqDistNodes(a, b, n);    % get nodes
     Ys = arrayfun(@(x) f(x), Xs); % get values at nodes
     Wn = sum(arrayfun(@(i) lagrPolyn(i, zeta, Xs)*Ys(i+1), 0:n)); % value of interpolation at zeta
-
-    pts = a:((b-a)/100):b;
-    hold on
-    plot(pts, arrayfun(@(x) f(x), pts))
-    plot(pts, arrayfun(@(x) sum(arrayfun(@(i) lagrPolyn(i, x, Xs)*Ys(i+1), 0:n)), pts))
-    hold off
 end
 
 function Pin = lagrPolyn(i, x, Xs)
@@ -25,7 +46,6 @@ function Pin = lagrPolyn(i, x, Xs)
     end
     Pin = 1;
     for it = [0:(i-1), (i+1):(length(Xs)-1)]
-%        fprintf('%f / %f\n', (x-Xs(it+1)), (Xs(i+1)-Xs(it+1)))
         Pin = Pin * (x-Xs(it+1)) / (Xs(i+1)-Xs(it+1));
     end
 end
@@ -33,15 +53,9 @@ end
 function [Xs, Wn] = monomialInterpolation(a, b, n, zeta)
     Xs = eqDistNodes(a, b, n);    % get nodes
     Ys = arrayfun(@(x) f(x), Xs); % get values at nodes
-    A = vander(Xs)                % create vandermonde matrix
-    coeffs = linsolve(A, Ys')     % solve A*coeffs = Ys
+    A = vander(Xs);               % create vandermonde matrix
+    coeffs = linsolve(A, Ys');    % solve A*coeffs = Ys
     Wn = polyval(coeffs, zeta);   % value of interpolation at zeta
-    
-    pts = a:((b-a)/100):b;
-    hold on
-    plot(pts, arrayfun(@(x) f(x), pts))
-    plot(pts, arrayfun(@(x) polyval(coeffs, x), pts))
-    hold off
 end
 
 function nodes = eqDistNodes(a, b, n)
