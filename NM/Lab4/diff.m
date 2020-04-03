592c04ea1dde389a2f2dede86d2b9e3b2f0aa743
herhor67 <herhor67@interia.pl>
2020-04-03 13:35:06 +0200

    Update diff.m

--- NM/Lab4/diff.m
+++ NM/Lab4/diff.m
@@ -1,10 +1,10 @@
 clear all
 clc
-n = 1
+n = 2
 x = 0
 h = 0.01
 
-%{
+
 a=0;
 b=10;
 pts = a:((b-a)/100):b;
@@ -14,18 +14,13 @@ ys = arrayfun(@(x) f(x), pts);
 plot(pts, ys);
 ys = arrayfun(@(x) forwdiff1(n, h, x), pts);
 plot(pts, ys);
-%ys = arrayfun(@(x) Reversediff1(n, h, x), pts);
-%plot(pts, ys);
 ys = arrayfun(@(x) forwdiff2(n, h, x), pts);
 plot(pts, ys);
-%ys = arrayfun(@(x) Reversediff2(n, h, x), pts);
-%plot(pts, ys);
 hold off
-%}
 
-d = forwdiff1(n, h, x)
 
-[d, h] = variateH(n, h, x, 0.00000001)
+d = forwdiff2(n, h, x)
+
 
 
 function y = f(x) 
@@ -114,24 +109,6 @@ function dydx = forwdiff1(n, h, z)
     dydx = 1/h*suma;
     
 end
-function dydx =  revdiff1(n, h, z)
-    mtrx = zeros(n+1, n+1);
-    for i = 0:n
-        mtrx(i+1, 1) = f(z-i*h);
-    end
-    for x = 1:n
-        for i = 0:(n-x)
-            mtrx(i+1, x+1) = (mtrx(i+1, x)-mtrx(i+2, x));
-        end
-    end
-    
-    suma = 0;
-    for i = 2:(n+1)
-        suma = suma + 1/(i-1)*mtrx(1,i);
-    end
-    dydx = 1/h*suma;
-    
-end
 
 function dydx = forwdiff2(n, h, z)
     mtrx = zeros(n+1, n+1);
@@ -146,24 +123,24 @@ function dydx = forwdiff2(n, h, z)
     
     suma = 0;
     for i = 3:(n+1)
-        suma = suma + (12-i+3)/12*(-1)^(i-1)*mtrx(1,i);
+        suma = suma + coefficients(i)*(-1)^(i)*mtrx(1,i);
     end
     dydx=1/h^2*suma;
 end
-function dydx =  revdiff2(n, h, z)
-    mtrx = zeros(n+1, n+1);
-    for i = 0:n
-        mtrx(i+1, 1) = f(z-i*h);
+
+function coeffs = coefficients(n)
+    mtrx = zeros(2*(n-1), 2);
+    mtrx(1,1) = 1;
+    mtrx(2*(n-1),1) = 1;
+    for i=2:(n-1)
+        mtrx(n-1-(i-2), 1) = 1/i*(-1)^(i+1);
+        mtrx(n+(i-2), 1)   = 1/i*(-1)^(i+1);
     end
-    for x = 1:n
-        for i = 0:(n-x)
-            mtrx(i+1, x+1) = (mtrx(i+1, x)-mtrx(i+2, x));
-        end
+    for i = 1:(n-1)
+        mtrx(i,2)= mtrx(2*(i-1)+1,1)*mtrx(2*(i-1)+2,1);
     end
-    
-    suma = 0;
-    for i = 3:(n+1)
-        suma = suma + (12-i+3)/12*mtrx(1,i);
+    coeffs = 0;
+    for i=1:n
+        coeffs = coeffs + mtrx(i,2);
     end
-    dydx =1/h^2*suma;
 end
