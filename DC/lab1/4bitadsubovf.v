96fbccfccf5b777bc5249226c105c4dcbf8e1c2a
herhor67 <herhor67@interia.pl>
2020-05-05 15:44:57 +0200

    Update 4bitadsubovf.v

--- DC/lab1/4bitadsubovf.v
+++ DC/lab1/4bitadsubovf.v
@@ -14,31 +14,32 @@ output OVF, CO;
 output [3:0] S; 
 input [3:0] A, B;
 input SUB;
+wire [3:0] Axrd;
 wire [2:0] Cint;
-XOR2 X1(A[0], A[0], SUB);
-XOR2 X2(A[1], A[1], SUB);
-XOR2 X3(A[2], A[2], SUB);
-XOR2 X4(A[3], A[3], SUB);
-FA A1(Cint[0], S[0], A[0], B[0], CI);
-FA A2(Cint[1], S[1], A[1], B[1], Cint[0]);
-FA A3(Cint[2], S[2], A[2], B[2], Cint[1]);
-FA A4(CO, S[3], A[3], B[3], Cint[2]);
+
+XOR2 X1(Axrd[0], A[0], SUB);
+XOR2 X2(Axrd[1], A[1], SUB);
+XOR2 X3(Axrd[2], A[2], SUB);
+XOR2 X4(Axrd[3], A[3], SUB);
+FA A1(Cint[0], S[0], Axrd[0], B[0], SUB);
+FA A2(Cint[1], S[1], Axrd[1], B[1], Cint[0]);
+FA A3(Cint[2], S[2], Axrd[2], B[2], Cint[1]);
+FA A4(CO,      S[3], Axrd[3], B[3], Cint[2]);
 XOR2 X5(OVF, CO, Cint[2]);
 endmodule 
 
 
 module ADD_4_OVF_TEST;
 reg [3:0] A, B;
-reg CI;
+reg SUB;
 wire [3:0] Y;
-wire CO;
-wire OVF;
+wire CO, OVF;
 
-ADD_4_OVF A4_1(OVF, CO, Y, A, B, CI);
+ADD_4_OVF A4_1(OVF, CO, Y, A, B, SUB);
 
 initial begin
     #100;
-    CI = 1'b0;
+    SUB = 1'b0;
     A = 4'b0000;
     B = 4'b0000;
     repeat (2) begin
@@ -46,14 +47,14 @@ initial begin
             repeat(16) #100 B = B + 1;
         #100 A = A + 1;
         end
-        #100 CI = ~CI;
+        #100 SUB = ~SUB;
     end
     $finish;
 end
 
 initial begin
     $monitor("%t: %b + %b = %b, C:%b, OVF:%b", $time, A, B, Y, CO, OVF);
-    $dumpfile("add_4_test.vcd");
+    $dumpfile("add_4_ovf_test.vcd");
     $dumpvars(0, ADD_4_OVF_TEST);
     $dumpon();
 end
