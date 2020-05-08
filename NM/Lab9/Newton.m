29f147d772c645b8cd42ff6119b16f27698d4b70
herhor67 <herhor67@interia.pl>
2020-05-08 14:51:35 +0200

    Update Newton.m

--- NM/Lab9/Newton.m
+++ NM/Lab9/Newton.m
@@ -7,29 +7,29 @@ e = 0.001;
 
 [x, n, c] = Newton(a, b, e)
 
-function y = f(x)
+function y = f(x) % function
     y = x^4 - 625;
 end
 
-function dy = df(x)
+function dy = df(x) % first der
     dy = 4 * x^3;
 end
 
-function ddy = ddf(x)
+function ddy = ddf(x) % second der
     ddy = 12 * x^2;
 end
 
 function [x, n] = Newton(a, b, e)
-    if f(a) * ddf(a) > 0
+    if f(a) * ddf(a) > 0 % choose beginning
         tempx = a;
     else
         tempx = b;
     end
-    x = tempx - f(tempx)/df(tempx);
+    x = tempx - f(tempx)/df(tempx);  % 1st iteration
     n=1;
-    while abs(x-tempx) > e
+    while abs(x-tempx) > e % repeat until desired accuracy is reached
         tempx = x;
-        x = tempx - f(tempx)/df(tempx);
+        x = tempx - f(tempx)/df(tempx); % nth iterations
         n = n + 1;
     end
 end
