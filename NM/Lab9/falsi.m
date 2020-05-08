6fcb23bcde2b1ddfbfb0b5ae952cd10eaddb8bed
herhor67 <herhor67@interia.pl>
2020-05-08 13:05:48 +0200

    Update falsi.m

--- NM/Lab9/falsi.m
+++ NM/Lab9/falsi.m
@@ -1,20 +1,22 @@
 clear all
 clc
 
-regulafalsi(-10, 1, 0.01)
+[x, d] = regulafalsi(-10, 1, 0.0000001)
 
 function y = f(x) 
     y = x^4 - 625;
 end
 
-function x = regulafalsi(a, b, acc)
+function [x, n] = regulafalsi(a, b, acc)
+    n = 0;
     if sign(f(a)) ~= sign(f(b))
         while 1
-            x = a - f(a)/(f(b)-f(a))*(b-a)
+            n = n+1;
+            x = a - f(a)/(f(b)-f(a))*(b-a);
             if sign(f(x)) == sign(f(a))
-                a = x
+                a = x;
             elseif sign(f(x)) == sign(f(b))
-                b = x
+                b = x;
             end
             if abs(f(x)) < acc
                 break;
