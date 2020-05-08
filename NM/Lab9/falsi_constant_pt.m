8b7f8d1460772ac5a468ab2defa60b7ba16f4ad6
herhor67 <herhor67@interia.pl>
2020-05-08 14:49:39 +0200

    Update falsi_constant_pt.m

--- NM/Lab9/falsi_constant_pt.m
+++ NM/Lab9/falsi_constant_pt.m
@@ -21,17 +21,17 @@ end
 
 function [x, n, c] =  regula_falsi(a, b, e)
     n = 1;
-    if f(a)*ddf(a) > 0
+    if f(a)*ddf(a) > 0 % check which border point should be assumed as constant
         c = a;
         tempx = b;
     else
         c = b;
         tempx = a;
     end
-    x = tempx - f(tempx)/(f(c) - f(tempx)) * (c - tempx);
-    while abs(x - tempx)> e
+    x = tempx - f(tempx)/(f(c) - f(tempx)) * (c - tempx); % 1st iteration
+    while abs(x - tempx) > e % repeat as long as desired accuracy is not satisfied
         n = n + 1;
         tempx = x;
-        x = tempx - f(tempx)/(f(c) - f(tempx)) * (c-tempx);
+        x = tempx - f(tempx)/(f(c) - f(tempx)) * (c-tempx); % nth iterations
     end
 end
