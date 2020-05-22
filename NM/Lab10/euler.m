e477b38cd879ac8dfa89744ac941f3337e72f979
herhor67 <herhor67@interia.pl>
2020-05-22 12:36:56 +0200

    Update euler.m

--- NM/Lab10/euler.m
+++ NM/Lab10/euler.m
@@ -1,26 +1,28 @@
 clear all
 clc
 
-a = 0;
-b = 1;
+e = 1;
 h = 0.1;
-yata = 1;
+d = 1;
 
-[Xs, Ys, Fxys, Dys] = diffeq(a, b, h, yata)
+[Xs, Ys, Fxys, Dys] = diffeq(0, e, d, h)
 
 
 function fxy = f(x, y)
-    fxy = 2 * x * y;
+    a = -5.8;
+    b = 4.7;
+    c = -0.9;
+    fxy = a * y + b * x + c * x * x;
 end
 
 
-function [Xs, Ys, Fxys, Dys] = diffeq(a, b, h, yata)
+function [Xs, Ys, Fxys, Dys] = diffeq(a, b, y0, h)
     Xs = a:h:b;
     Ys   = zeros(1, length(Xs));
     Fxys = zeros(1, length(Xs));
     Dys  = zeros(1, length(Xs));
     
-    Ys(1) = yata;
+    Ys(1) = y0;
     Fxys(1) = f(Xs(1), Ys(1));
     Dys(1) = h * Fxys(1);
     
