3e2925d21d0c2f9180d091d428b8119d8f389656
herhor67 <herhor67@interia.pl>
2020-06-12 12:40:07 +0200

    Update net.m

--- NM/Lab11/net.m
+++ NM/Lab11/net.m
@@ -8,21 +8,24 @@ a2 = 0.01;
 h = 0.2;
 r = 0.1;
 
-w = net_method(tMax, zMin, zMax, h, r)
+w = net_method(tMax, zMin, zMax, h, r, a2)
 
 
 
-function w = net_method(tM, zm, zM, h, r)
-    Ti = Nodes(0,  tM, h)
-    Zk = Nodes(zm, zM, r)
-    w=0
+function w = net_method(tM, zm, zM, h, r, a2)
+    [Ti, tc] = Nodes(0,  tM, h)
+    [Zk, zc] = Nodes(zm, zM, r)
+    w = zeros(length(Ti), length(Zk));
+    for i = 1
+    end
 end
 
 function ret = wt0z(z)
     ret = sin(pi*z)
 end
 
-% Separate interval into h-long pieces, return their borders
-function [nodes] = Nodes(a, b, h)
+% Separate interval into h-long pieces, return their borders and count
+function [nodes, count] = Nodes(a, b, h)
     nodes = a:h:b;
+    count = length(nodes);
 end
